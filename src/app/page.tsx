0847188fa17778fc0dcd6525ca8e1e5ff92f71b8
Keerthanams7799 <keerthanams@techverse.world>
2024-04-01 21:59:57 +0530

    working on layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,37 +12,79 @@ import {
 } from "@clerk/nextjs";
 import { useMutation, useQuery } from "convex/react";
 import { api } from "../../convex/_generated/api";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+ 
+const formSchema = z.object({
+  title: z.string().min(1).max(200),
+})
 
 export default function Home() {
   const organization = useOrganization();
   const user = useUser();
 
-  let orgId : string | undefined
-  if(organization.isLoaded && user.isLoaded) {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+    },
+  })
+
+  let orgId: string | undefined;
+  if (organization.isLoaded && user.isLoaded) {
     orgId = organization.organization?.id ?? user.user?.id;
   }
 
-  const files = useQuery(api.files.getFiles, orgId ? { orgId} : "skip");
+  const files = useQuery(api.files.getFiles, orgId ? { orgId } : "skip");
   const createFile = useMutation(api.files.createFile);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="container mx-auto pt-12">
+      <div className="flex justify-between items-center">
+        <h1 className="text-4xl font-bold tracking-tight text-gray-900">
+          Your Files
+        </h1>
 
+        <Dialog>
+          <DialogTrigger asChild>
+          <Button
+          onClick={() => {
+            if (!orgId) return;
+            createFile({
+              name: "hello world",
+              orgId,
+            });
+          }}
+        >
+          Upload File
+        </Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Are you absolutely sure?</DialogTitle>
+              <DialogDescription>
+                This action cannot be undone. This will permanently delete your
+                account and remove your data from our servers.
+              </DialogDescription>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
+
+       
+      </div>
       {files?.map((file) => {
         return <div key={file._id}>{file.name}</div>;
       })}
-
-      <Button
-        onClick={() => {
-          if (!orgId) return;
-          createFile({
-            name: "hello world",
-            orgId,
-          });
-        }}
-      >
-        Click Me
-      </Button>
     </main>
   );
 }
