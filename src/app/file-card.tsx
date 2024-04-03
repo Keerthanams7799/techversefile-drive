d282bcad90d744c1150a64cb14c0497e8ec22e75
Keerthanams7799 <keerthanams@techverse.world>
2024-04-03 20:19:12 +0530

    Delete dropdown option

---
+++ src/app/file-card.tsx
@@ -0,0 +1,96 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Doc } from "../../convex/_generated/dataModel";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
+import { DeleteIcon, MoreVertical, Trash2Icon } from "lucide-react";
+import { useState } from "react";
+
+function FileCardActions() {
+  const [isConfirmOpen, setIsConfirmOpen] = useState(false);
+  return (
+    <>
+      <AlertDialog open={isConfirmOpen} onOpenChange={setIsConfirmOpen}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action will mark the file for our deletion process. Files are
+              deleted periodically.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction
+              onClick={() => {
+                // TODO: actually  delete the file here
+              }}
+            >
+              Continue
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <MoreVertical />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem
+            onClick={() => setIsConfirmOpen(true)}
+            className="flex gap-1 text-red-600 items-center cursor-pointer"
+          >
+            <Trash2Icon className="w-4 h-4" />
+            Delete
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
+}
+
+export function FileCard({ file }: { file: Doc<"files"> }) {
+  return (
+    <Card>
+      <CardHeader className="relative">
+        <CardTitle className="flex gap-2 text-base font-normal">
+          <div className="flex justify-center"></div>
+          {file.name}
+        </CardTitle>
+        <div className="absolute top-2 right-2">
+          <FileCardActions />
+        </div>
+        {/* <CardDescription>Card Description</CardDescription> */}
+      </CardHeader>
+      <CardContent>
+        <p>Card Content</p>
+      </CardContent>
+      <CardFooter>
+        <Button>Download</Button>
+      </CardFooter>
+    </Card>
+  );
+}
